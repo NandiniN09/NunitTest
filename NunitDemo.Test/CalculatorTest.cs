f6624c1b35a2a0a2f192bd954b5fab275ec98dc2
Nandini <nandininarayanan96@gmail.com>
2021-05-09 19:46:49 -0400

    Add project files.

---
+++ NunitDemo.Test/CalculatorTest.cs
@@ -0,0 +1,42 @@
+﻿using Nunit;
+using NUnit.Framework;
+using System;
+
+namespace NunitDemo.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test, Order(2)]
+        public void Test_Addition_With_Valid_Intigers()
+        {
+            var calc = new Calculator();
+           var result=  calc.Addition(3, 5);
+            Assert.AreEqual(8, result);
+        }
+
+        [Test, Order(1)]
+        public void Test_Subtraction_Argument_Exception()
+        {
+            var calc = new Calculator();
+            Assert.Catch<SystemException>(() => calc.Subtraction(4, 5));
+            Assert.Throws<ArgumentException>(() => calc.Subtraction(4, 5));
+        }
+
+        [TestCase(1,2,3)]
+        [TestCase(2, 3, 5)]
+        [TestCase(3, 4, 7)]
+        public void Test_Addition_Multiple(int first, int second, int expectedresult)
+        {
+            var calc = new Calculator();
+            var calculated = calc.Addition(first, second);
+            Assert.AreEqual(expectedresult, calculated);
+
+        }
+        [Ignore("Ignore Test")]
+        public void Test_To_Ignore()
+        {
+
+        }
+    }
+}
